1b08cceb6ad96b2435912fb426dab8d0c73b70f3
aanhtuan <tuanna.23it@vku.udn.vn>
2025-04-12 00:16:15 +0700

    fix

---
+++ app/src/main/java/com/sporticast/model/Book.kt
@@ -0,0 +1,9 @@
+package com.sporticast.model
+data class Book(
+    val title: String,
+    val author: String,
+    val duration: String,
+    val imageUrl: String,
+    val rating: Float,
+    val listenCount: Int
+)
--- app/src/main/java/com/sporticast/model/Category.kt
+++ app/src/main/java/com/sporticast/model/Category.kt
@@ -1,7 +1,6 @@
 package com.sporticast.model
 
 data class Category(
-    var id: Int? = null,
-    var name: String? = null,
-    var createdAt: String? = null
+    val name: String,
+    val icon: String
 )
